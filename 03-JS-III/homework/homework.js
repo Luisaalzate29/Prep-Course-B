386b57aa983bb0473ced2bf17d66c508ea6b7110
Luisa Fernanda Alzate Echeverri <luisaalzate@cobelen.local>
2022-11-02 12:29:23 -0500

    home 2 3 y 4

--- 03-JS-III/homework/homework.js
+++ 03-JS-III/homework/homework.js
@@ -4,33 +4,78 @@
 // Pista: "console.log()" NO fucionará.
 // No cambies los nombres de las funciones.
 
+const { suma } = require("../../02-JS-II/homework/homework");
+
 function imprimirSumaNumeros() {
     // Imprime la suma de los números del 1 al 10
     // Pista: usa un acumulador
     // Tu código:
+
+    var suma=0
+
+    for (var i = 1 ; i < 11 ; i++){
+      suma = suma + i
+    }
+
+    return suma
+
+
 }
 
 function encuentraPares(array){
   // Devuelve un arreglo con los pares encontrados
   // en el arreglo de enteros pasado como parámetro
   // Tu código:
-}
+
+ 
+var nuevoarray = []
+
+for ( var i = 0; i < array.length ; i++){
+  if ( array [i] % 2 === 0 ){
+    nuevoarray.push ( array [i])
+  }
+ }
+return nuevoarray
+  
+  }
+
 
 function elevaAlCuadrado(array){
   // Devuelve un arreglo con cada número del array
   // elevado al cuadrado
   // Tu código:
-}
+    
+
+  return array.map((e) => e**2)
+ 
+   }
+  
+
+     
+
 
 function sumaArray(array){
   // Devuelve el resultado de sumar todos los elementos
   // de un arreglo de enteros dado
   // Tu código:
+
+
+ var suma = 0;
+
+for( var i = 0; i < array.length ; i++){
+  suma = suma + array[i]
+}
+return suma
+  
 }
 
 function numeroDigitos(num){
   // Devuelve el número de dígitos de un número dado
   // Tu código:
+
+ var numero = num.toString();
+
+ return numero.length
 }
   
   
